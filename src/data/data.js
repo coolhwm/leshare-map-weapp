adc307e4305d2f3860b1f28397ca777f9064fb9c
coolhwm <coolhwm@qq.com>
2017-07-16 15:32:56 +0800

    完成开发

---
+++ src/data/data.js
@@ -0,0 +1,58 @@
+export const data = [
+  {
+    'seq': '1',
+    'name': '莆田西征管所网点',
+    'address': '莆田市城厢区华亭镇山牌村莆田西征管所办公楼',
+    'phone': '13700000000',
+    'latitude': '25.399509',
+    'longitude': '118.977344',
+    'preview_image': 'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg?imageView2/1/w/120/h/120/format/jpg/q/75|imageslim',
+    'images': [
+      'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg',
+      'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg',
+      'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg'
+    ]
+  },
+  {
+    'seq': '2',
+    'name': '仙游城区征管所网点',
+    'address': '莆田市仙游县鲤南镇下楼村高速公路仙游城区征管所办公楼',
+    'phone': '13700000000',
+    'latitude': '25.376232',
+    'longitude': '118.735901',
+    'preview_image': 'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg?imageView2/1/w/120/h/120/format/jpg/q/75|imageslim',
+    'images': [
+      'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg',
+      'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg',
+      'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg'
+    ]
+  },
+  {
+    'seq': '3',
+    'name': '涵江江口征管所网点',
+    'address': '莆田市涵江区涵江区江口镇新前村高速公路涵江江口征管所办公楼',
+    'phone': '13700000000',
+    'latitude': '25.492494',
+    'longitude': '119.188597',
+    'preview_image': 'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg?imageView2/1/w/120/h/120/format/jpg/q/75|imageslim',
+    'images': [
+      'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg',
+      'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg',
+      'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg'
+    ]
+  },
+  {
+    'seq': '4',
+    'name': '市行政服务中心网点',
+    'address': '莆田市荔城区荔城中大道1998号3号楼市行政大厅85号窗口',
+    'phone': '13700000000',
+    'latitude': '25.451307',
+    'longitude': '119.008712',
+    'preview_image': 'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg?imageView2/1/w/120/h/120/format/jpg/q/75|imageslim',
+    'images': [
+      'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg',
+      'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg',
+      'http://ostb6zm4z.bkt.clouddn.com/etc-logo.jpg'
+    ]
+  }
+];
